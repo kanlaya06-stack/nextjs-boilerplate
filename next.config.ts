c176d7d8d4bb1842f824600760018f47c6e7a6e2
kanlaya06-stack <k.inaod06@gmail.com>
2026-08-29 12:23:56 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextModeConfig} */
+const nextConfig = {
   typescript: {
+    // ป้องกันไม่ให้ build ล้มเหลวหากมี type เล็กน้อยหลุด
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ['images.unsplash.com'],
   },
 };
 
